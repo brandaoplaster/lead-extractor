d0ea5c00885ec5406292a147a5384642f1ebbe79
brandaoplaster <brandaoplaster@gmail.com>
2025-11-21 21:17:36 -0300

    feat: Add customer controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [ :show ]
+  def index
+    @customers = Customer.all
+  end
+
+  def show;end
+
+  private
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+end
